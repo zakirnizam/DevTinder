864ac013202d33dc8d08cef29c06b5eb95fb0b13
Zakir Nizam <mohammadzakirnizam@gmail.com>
2025-07-27 13:46:48 +0530

    Playing with route handlers

--- src/app.js
+++ src/app.js
@@ -1,22 +1,29 @@
 const express = require("express");
 const app = express();
 
+app.use(
+  "/user",
+ [(req, res, next) => {
+    console.log("Route handler 1");
+    // res.send("route handler 1 executed");
 
+    next();
+  },
+  (req, res, next) => {
+    console.log("Route handler 2");
+    next();
 
-app.get('/user',(req,res)=>{
-res.send({"Name":"Nizam","Gender":"male"})
-});
+  },] ,
+  (req, res, next) => {
+    console.log("Route handler 3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Route handler 4");
+    res.send("route handler 4 executed");
 
-app.post('/user',(req,res)=>{
-
-res.send("Added new users to DB")
-});
-
-app.delete('/user',(req,res)=>{
-
-res.send("Deleted a user from DB")
-})
-
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");

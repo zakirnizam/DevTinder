6ee85869b78128ca8b5690d862fa002cd57884e7
Zakir Nizam <mohammadzakirnizam@gmail.com>
2025-07-28 07:43:12 +0530

    Episode 5 | Error Handling

--- src/app.js
+++ src/app.js
@@ -1,24 +1,21 @@
 const express = require("express");
 const app = express();
 
-const {adminAuth,userAuth} = require("./middleware/auth");
+app.get("/getUserData", (req, res) => {
+  // Use try catch everywher, its a good coding practice
+  try {
+    throw new Error("Some error here");
+  } catch (error) {
+    res.status(500).send("Something went wrong | CATCH BLOCK");
+  }
+});
 
-
-app.use('/admin',adminAuth);
-
-app.use('/user',userAuth,(req,res)=>{
-  console.log("User Data");  
-})
-
-app.get('/admin/getAlldata',(req,res)=>{
-  console.log("Access to all the data");
-}
-)
-
-app.get('/admin/deleteData',(req,res)=>{
-  console.log("Delete all the data");
-}
-)
+// Use This As a Wildcard , It will be triggered when there are unhandled Errors
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.send("Something Went Wrong , Please Try Again Sometime");
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");

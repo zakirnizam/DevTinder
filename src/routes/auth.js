425ce86dd1217d67da1ce9d4fe3f698febf7f607
Mohammad Zakir Nizam <jakir.nizam@niveussolutions.com>
2025-12-01 11:40:08 +0530

    EP 11 | Created Logout API

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -37,7 +37,6 @@ authRouter.post("/login", async (req, res) => {
     }
     const isPasswordValid = await user.validatePassword(password);
     if (isPasswordValid) {
-        console.log("nizam")
       //Creare a JWT token
       const token = await user.getJWT();
       res.cookie("token", token, {expires: new Date(Date.now() + 86400000)
@@ -50,4 +49,11 @@ authRouter.post("/login", async (req, res) => {
     res.status(400).send("ERROR :" + error.message);
   }
 });
+
+authRouter.post("/logout", async (req, res) => {
+res.cookie('token',null,{
+  expires: new Date(Date.now())
+});
+res.send("Logout Successful");
+})
 module.exports = authRouter;

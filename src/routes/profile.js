ee5d2df18ef1c506b2d0215de1ba10ff6a5f01ba
Zakir Nizam <mohammadzakirnizam@gmail.com>
2025-12-03 12:47:32 +0530

    EP 11 | Created Edit profile API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,14 +1,33 @@
 const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middleware/auth");
+const { validateEditProfileData } = require("../utils/validator");
 
 
-profileRouter.get("/profile",userAuth, async(req, res) => {
+profileRouter.get("/profile/view",userAuth, async(req, res) => {
   try{
 const user = req.user;
   res.send(user);}
   catch (error) {
     res.status(400).send("ERROR" + error.message);
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req))
+      throw new Error("Invalid Edit Request");
+    const loggedInUser = req.user;
+    console.log(loggedInUser);
+
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+    console.log(loggedInUser);
+    res.json({
+      message: `${loggedInUser.lastName} Your Profile Updated Succesfuly`,
+      data: loggedInUser,
+    });
+  } catch (error) {
+    res.status(400).send("ERROR :" + error.message);
   }
 });
 
